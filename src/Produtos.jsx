ffef5c552cf135a8b3d4d422a20a78250dcf3b66
BernardCoutinho <80927497+BernardCoutinho@users.noreply.github.com>
2021-11-26 19:42:37 -0300

    produtos aparecendo na tela

--- src/Produtos.jsx
+++ src/Produtos.jsx
@@ -1,49 +1,33 @@
 import React from "react";
 import api from "./api";
 import { Link } from "react-router-dom";
+import Item from "./Item";
+import ProdutoComponent from "./ProdutoComponent";
 
+function Produtos (){
+    const [item, setItem] =  React.useState([]);
 
-function Produtos(){
-    const [produtos, setProdutos] =  React.useState([]);
-    const [quantidade, setQuantidade] = React.useState();
+    React.useEffect(verifica, []);
 
-    React.useEffect(consultar, []);
+    function verifica(){
+     const listar = (res) => {
+         console.dir(res.data)
+        setItem(res.data);
+     }
 
-    function consultar(){
-     const listar = (res) => setProdutos(res.data);
-        api.get("/produtos").then(listar);
+        api.get("/produto").then(listar);
     }
-    
-    const handleSubmit = (event) => {
-        event.preventDefault();
-    }
 
-return (
-    <div>
-        <h1>Produtos</h1>
-        {produtos.map((p) => (
-          <li key={p.id}>{p.title} <Link to={`produtos/${p.id}`}> </Link> 
-            <div className="card" style="width: 18rem;">
-                <img className="card-img-top" src="..." alt="Card image cap" />
-                <div className="card-body">
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
-            </div>
-        </li>))}
-        <form onSubmit={handleSubmit}>
-            <label style={{margin:"10px"}} htmlFor="">Qntd</label>
-            <input 
-                style={{width:"4rem", margin:"10px"}}
-                type="number"
-                nome="quantidade"
-                id="quantidade"
-                value={quantidade}
-                onChange={(event) => setQuantidade(1)}
-                placeholder="0" />
-        </form>
-        <button type="submit">Enviar</button>
-    </div>
-   )
+    return (
+        <div>
+            <h1>Produtos</h1>
+           <ul>
+                {item.map((i) => (
+                    <ProdutoComponent key={i.id} produto= {i} />
+             ))}
+                </ul>
+        </div>
+       )
 }
 
 export default Produtos;
