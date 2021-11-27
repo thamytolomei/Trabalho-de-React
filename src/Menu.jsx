4a3855d162152b26524255a2b73526665536b864
BernardCoutinho <80927497+BernardCoutinho@users.noreply.github.com>
2021-11-27 14:00:19 -0300

    Tentando ajeitar os links do menu

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -2,16 +2,16 @@ function Menu() {
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-          <a style={{padding: "10px", listStyle:"none"}} className="navbar-brand" href="#">
+          <a style={} className="navbar-brand" href="#">
           SerranoTec Eletrônicos
           </a>
-          <a style={{padding: "10px", listStyle:"none"}} className="navbar-brand" href="#">
+          <a style={} className="navbar-brand" href="#">
             <button Link to = {"/home"}> Home</button>
           </a>
-          <a style={{padding: "10px", listStyle:"none"}} className="navbar-brand" href="#">
+          <a style={} className="navbar-brand" href="#">
           <button Link to = {"/produtos"}>Produtos</button>
           </a>
-          <a style={{padding: "10px", listStyle:"none"}} className="navbar-brand" href="#">
+          <a style={} className="navbar-brand" href="#">
           <button Link to = {"/carrinho"}> Carrinho </button>
           </a>
         </div>
