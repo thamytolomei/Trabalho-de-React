b3aa275f9ffe3f93ce1d2aa948c9f787732defd9
Gustavo Kozlowski <94485093+GustavoKozlowski@users.noreply.github.com>
2021-11-26 22:51:37 -0300

    Criação do componente "Carrinho"

--- src/Carrinho.jsx
+++ src/Carrinho.jsx
@@ -1,9 +1,25 @@
 import React from "react";
-
+import { useState, useEffect } from "react";
+import api from "./api";
 
 function Carrinho(){
+    const [ carrinho, setCarrinho] = useState([]);
 
+    useEffect(() => {
+        api.get("/produtos").then(exibir);
+        console.log("oi");
+        function exibir(response) {
+          setCarrinho(response.data);
+          console.log(response.data);
+        }
+      }, []);
+
+
+   
 return 
+        
+        
+        
 }
 
 export default Carrinho;
